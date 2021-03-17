06fb61ea0f35d7fd7d337e8651aa0aa8a3f52e33
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-17 15:32:07 +0700

    17/03/21

---
+++ Praktik/Pertemuan 1/Searching and Sorting In An Integer List/src/IntegerList.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Icad
+ */
+public class IntegerList {
+    
+    int[] list;
+    
+    public IntegerList(int size){
+        
+        list = new int[size];
+    }
+    
+    public void randomize(){
+        for (int i = 0; i < list.length; i++){
+            list[i] = (int)(Math.random() * 100) + 1;
+        }
+    }
+    
+    public void print(){
+        for (int i = 0; i < list.length; i++){
+            System.out.println(i + ":\t" + list[i]);
+        }
+    }
+    
+    public int search(int target){
+        int location = -1;
+        for(int i = 0; i < list.length && location == -1; i++){
+            if(list[i]==target){
+                location = i;
+            }
+        }
+        return location;
+    }
+    
+    public void selectionSort(){
+        int minIndex;
+        
+        for(int i = 0; i < list.length-1; i++){
+            minIndex = i;
+            for(int j = i + 1; j < list.length; j++){
+                if(list[j] < list[minIndex]){
+                    minIndex = j;
+                }
+            }
+            
+            int temp = list[i];
+            
+            list[i] = list[minIndex];
+            list[minIndex] = temp;
+        }
+    }
+    
+    //1. add method replaceFirst(int oldVal, int newVal)
+    public void replaceFirst(int oldVal, int newVal){
+        boolean found = false;
+        int valLoc = search(oldVal);
+        
+        if (valLoc >= 0){
+            found = true;
+            list[valLoc] = newVal;
+        }
+        
+    }
+    
+}
---
+++ Praktik/Pertemuan 1/Searching and Sorting In An Integer List/src/IntegerListTest.java
@@ -0,0 +1,86 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Icad
+ */
+public class IntegerListTest {
+    
+    static IntegerList list = new IntegerList(10);
+    static Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+        printMenu();
+        int choice = scan.nextInt();
+        
+        while (choice != 0){
+            dispatch(choice);
+            printMenu();
+            choice = scan.nextInt();
+        }
+    }
+    
+    public static void dispatch(int choice){
+        int loc, oldVal, newVal;
+        switch (choice){
+            case 0 :
+                System.out.println("Bye!");
+                break;
+                
+            case 1 : 
+                System.out.println("How big the list should be?");
+                int size = scan.nextInt();
+                list = new IntegerList(size);
+                list.randomize();
+                break;
+                
+            case 2 :
+                list.selectionSort();
+                break;
+                
+            case 3 :
+                System.out.println("Enter the value to look for: ");
+                loc = list.search(scan.nextInt());
+                if (loc != -1)
+                    System.out.println("Found at location " + loc);
+                else
+                    System.out.println("Not in list");
+                break;
+                
+            case 4 : 
+                list.print();
+                break;
+                
+            case 5 :
+                System.out.println("Enter old Value: ");
+                oldVal = scan.nextInt();
+                System.out.println("Enter new Value: ");
+                newVal = scan.nextInt();
+                list.replaceFirst(oldVal, newVal);
+                break;
+                
+            default :
+                System.out.println("Sorry, invalid choice!");
+        }
+    }
+    
+    public static void printMenu(){
+        System.out.println("\n      Menu        ");
+        System.out.println("    ==============  ");
+        System.out.println("0. Quit");
+        System.out.println("1. Create a new list (** do this first! **)");
+        System.out.println("2. Sort the list using selection sort");
+        System.out.println("3. Find an element in the list using linear search");
+        System.out.println("4. Print the list");
+        System.out.println("5. Find and replace");
+        System.out.println("\nEnter your choice: ");
+    }
+}
