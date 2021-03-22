861e60d37f161f7a98986faa069bc45ee877281b
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-22 23:45:33 +0700

    Completed--Placing Exception Handler

---
+++ Praktik/Pertemuan 2/Placing Exception Handler/src/ParseInts.java
@@ -0,0 +1,33 @@
+// ****************************************************************
+// ParseInts.java
+//
+// Reads a line of text and prints the integers in the line.
+//
+// ****************************************************************
+
+import java.util.Scanner;
+
+public class ParseInts {
+
+    public static void main(String[] args) {
+        int val, sum = 0;
+        Scanner scan = new Scanner(System.in);
+        String line;
+        System.out.println("Enter a line of text");
+        Scanner scanLine = new Scanner(scan.nextLine());
+
+        while (scanLine.hasNext()) {
+            
+            //write the try-catch inside the loop for not terminating if catching an exception
+            try {
+                
+                val = Integer.parseInt(scanLine.next());
+                sum += val;
+            } 
+            catch (NumberFormatException e) {
+            }
+        }
+        
+        System.out.println("The sum of the integers on this line is " + sum);
+    }
+}
