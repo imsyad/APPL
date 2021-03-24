b9c3bee5b08c7612cd5625501ca1c5643ebd3af4
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-24 09:29:59 +0700

    Second 15 minutes

--- Praktik/Pertemuan 2/Copying a File/src/Files.java
+++ Praktik/Pertemuan 2/Copying a File/src/Files.java
@@ -5,11 +5,31 @@
 //
 // ****************************************************************
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Files {
     
-    public void openFile(String fileName) throws FileNotFoundException{
+    private String fileName;    
+    private final String filePath = "F:\\Tugas\\Semester 4\\APPL\\Praktik\\Pertemuan 2\\Copying a File\\filetest";
+    
+    //Construct Files object
+    public Files(String fileName){
+        
+        this.fileName = fileName;
+    }
+    
+    public void openFile() throws FileNotFoundException{
+        
+        //open file using file path and file name
+        File openedFile = new File(this.filePath + this.fileName);
+        
+        //if file doesn't exist throw exception
+        if (!openedFile.exists())
+            throw new FileNotFoundException("File not found!");
+    }
+    
+    public void read(){
         
     }
 }
