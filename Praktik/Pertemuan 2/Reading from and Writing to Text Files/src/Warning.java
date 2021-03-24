35c8c1fce1fa100724d95b7a78c8af72d3c4cbc8
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-24 19:07:18 +0700

    Second 15 minutes

--- Praktik/Pertemuan 2/Reading from and Writing to Text Files/src/Warning.java
+++ Praktik/Pertemuan 2/Reading from and Writing to Text Files/src/Warning.java
@@ -22,23 +22,47 @@ public class Warning {
         String outputName = "warning.dat";
         String path = "F:\\Tugas\\Semester 4\\APPL\\Praktik\\Pertemuan 2\\Reading from and Writing to Text Files\\file test\\"; //Folder path
         try {
-        // Set up scanner to input file
+            // Set up scanner to input file
 //        BufferedReader br = new BufferedReader(new FileReader(path + inputName));
-        File inFile = new File(path + inputName);
-        Scanner scan = new Scanner (inFile);
-        
-        // Set up the output file stream
-        PrintWriter outFile = new PrintWriter(inFile);
+            File inFile = new File(path + inputName);
+            Scanner scan = new Scanner(inFile);
 
-// Print a header to the output file
+            // Set up the output file stream
+            PrintWriter outFile = new PrintWriter(outputName);
+//        FileWriter fw = new FileWriter(path + outputName);
+
+            // Print a header to the output file
             outFile.println();
             outFile.println("Students on Academic Warning");
             outFile.println();
-// Process the input file, one token at a time
-            while () {
-// Get the credit hours and quality points and
-// determine if the student is on warning. If so,
-// write the student data to the output file.
+            // Process the input file, one token at a time
+            while (scan.hasNext()) {
+                line = scan.nextLine();
+                System.out.println(line);
+
+                // Get the credit hours and quality points and
+                String[] split = line.split(" ");
+                name = split[0];
+                creditHrs = Integer.parseInt(split[1]);
+                qualityPts = Double.parseDouble(split[2]);
+                gpa = qualityPts / creditHrs;
+
+                // determine if the student is on warning. If so,
+                // write the student data to the output file.
+                if (creditHrs < 30) {
+                    if (gpa < 1.5) {
+                        outFile.write(name + " " + creditHrs + " " + gpa);
+                    }
+                } else if (creditHrs < 60) {
+                    if (gpa < 1.75) {
+                        outFile.write(name + " " + creditHrs + " " + gpa);
+                    }
+                } else {
+                    if (gpa < 2.0) {
+                        outFile.write(name + " " + creditHrs + " " + gpa);
+                    }
+                }
+
             }
 // Close output file
         } catch (FileNotFoundException exception) {
