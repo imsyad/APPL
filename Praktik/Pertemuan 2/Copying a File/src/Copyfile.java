e73d967eec3d2f89d546610cb31f387c48315fa1
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-24 10:01:08 +0700

    Completed--Copiying a file

--- Praktik/Pertemuan 2/Copying a File/src/Copyfile.java
+++ Praktik/Pertemuan 2/Copying a File/src/Copyfile.java
@@ -1,7 +1,7 @@
 // ****************************************************************
 // Copyfile
 //
-// Main file.
+// Opening file, check wether file exist or not, and print the file.
 //
 // ****************************************************************
 
@@ -10,11 +10,48 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Copyfile {
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(System.in);
-        String fileName = scan.nextLine();
-        
-        
+        boolean found = false; //Variable to validate the existence of the file
+
+        while (!found) {
+            System.out.print("File name: ");
+            
+            //Get file name
+            String fileName = scan.nextLine();
+            
+            try {
+                //Create new File object with path and file name as the parameter
+                File myFile = new File("F:\\Tugas\\Semester 4\\APPL\\Praktik\\Pertemuan 2\\Copying a File\\filetest\\" + fileName);
+
+                //If file doesn't exist, throw exception
+                if (!myFile.exists()) {
+                    throw new FileNotFoundException("File not found!");
+                }
+
+                //File exist, start to print the file
+                Scanner readFile = new Scanner(myFile);
+                
+                while (readFile.hasNextLine()) {
+                    
+                    //Get the line
+                    String line = readFile.nextLine();
+                    
+                    //Print the line
+                    System.out.println(line);
+                }
+                
+                //If file exist and printable, stop the loop
+                found = true;
+            } 
+            catch (FileNotFoundException e) {
+                
+                //Print exception message
+                System.err.println(e.getMessage());
+            }
+
+        }
+
     }
 }
