60db85591d388e608fef8b5ff3902858dedd389c
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-24 09:13:04 +0700

    First 15 minutes

---
+++ Praktik/Pertemuan 2/Copying a File/src/Copyfile.java
@@ -0,0 +1,20 @@
+// ****************************************************************
+// Copyfile
+//
+// Main file.
+//
+// ****************************************************************
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Copyfile {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String fileName = scan.nextLine();
+        
+        
+    }
+}
---
+++ Praktik/Pertemuan 2/Copying a File/src/Files.java
@@ -0,0 +1,15 @@
+// ****************************************************************
+// Files.java
+//
+// Provides file method.
+//
+// ****************************************************************
+
+import java.io.FileNotFoundException;
+
+public class Files {
+    
+    public void openFile(String fileName) throws FileNotFoundException{
+        
+    }
+}
