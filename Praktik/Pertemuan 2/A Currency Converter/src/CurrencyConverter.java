e0b4bbd81fea5e66e9357d36408b2d0be2d7a427
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-25 13:53:48 +0700

    First 15 minutes

---
+++ Praktik/Pertemuan 2/A Currency Converter/src/CurrencyConverter.java
@@ -0,0 +1,21 @@
+// ***********************************************************************
+// CurrencyConverter.java
+//
+// Computes the dollar value of the cost of an item in another currency.
+// ***********************************************************************
+
+import java.awt.*;
+import javax.swing.*;
+
+public class CurrencyConverter {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Currency Converter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        RatePanel ratePanel = new RatePanel();
+        frame.getContentPane().add(ratePanel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
---
+++ Praktik/Pertemuan 2/A Currency Converter/src/RatePanel.java
@@ -0,0 +1,62 @@
+// ******************************************************************
+// RatePanel.java
+//
+// Panel for a program that converts different currencies to
+// U.S. Dollars
+// ******************************************************************
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class RatePanel extends JPanel {
+
+    private double[] rate; // exchange rates
+    private String[] currencyName;
+    private JLabel result;
+// ------------------------------------------------------------
+// Sets up a panel to convert cost from one of 6 currencies
+// into U.S. Dollars. The panel contains a heading, a text
+// field for the cost of the item, a combo box for selecting
+// the currency, and a label to display the result.
+// ------------------------------------------------------------
+
+    public RatePanel() {
+        JLabel title = new JLabel("How much is that in dollars?");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        title.setFont(new Font("Helvetica", Font.BOLD, 20));
+        // Set up the arrays for the currency conversions
+        currencyName = new String[]{"Select the currency..",
+            "European Euro", "Canadian Dollar",
+            "Japanese Yen", "Australian Dollar",
+            "Indian Rupee", "Mexican Peso"};
+        
+        JComboBox currency = new JComboBox(currencyName);
+     
+        currency.addActionListener(new ComboListener());
+        
+        rate = new double[]{0.0, 1.2103, 0.7351,
+            0.0091, 0.6969,
+            0.0222, 0.0880};
+        result = new JLabel(" ------------ ");
+        add(title);
+        add(result);
+    }
+// ******************************************************
+// Represents an action listener for the combo box.
+// ******************************************************
+
+    private class ComboListener implements ActionListener {
+// --------------------------------------------------
+// Determines which currency has been selected and
+// the value in that currency then computes and
+// displays the value in U.S. Dollars.
+// --------------------------------------------------
+
+        public void actionPerformed(ActionEvent event) {
+            int index = 0;
+            result.setText("1 " + currencyName[index]
+                    + " = " + rate[index] + " U.S. Dollars");
+        }
+    }
+}
