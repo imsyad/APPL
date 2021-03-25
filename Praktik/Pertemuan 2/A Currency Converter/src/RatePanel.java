df73d1cf5f9170fafd369395f3d5fe4f68f29418
Icad <irsyadmuhammad.020201@gmail.com>
2021-03-25 18:49:20 +0700

    Eighth 15 mintues

--- Praktik/Pertemuan 2/A Currency Converter/src/RatePanel.java
+++ Praktik/Pertemuan 2/A Currency Converter/src/RatePanel.java
@@ -15,6 +15,7 @@ public class RatePanel extends JPanel {
     private String[] currencyName;
     private JLabel result;
     private JComboBox currency;
+    private JTextField inputCurrency;
 // ------------------------------------------------------------
 // Sets up a panel to convert cost from one of 6 currencies
 // into U.S. Dollars. The panel contains a heading, a text
@@ -36,11 +37,17 @@ public class RatePanel extends JPanel {
         currency.setBounds(50,50,90,20);    
         currency.addActionListener(new ComboListener());
         
+        JLabel inputLabel = new JLabel("Input: ");
+        inputLabel.setBounds(50, 50, 90, 20);
+        inputCurrency = new JTextField(10);
+        
         rate = new double[]{0.0, 1.2103, 0.7351,
             0.0091, 0.6969,
             0.0222, 0.0880};
         result = new JLabel(" ------------ ");
         add(title);
+        add(inputLabel);
+        add(inputCurrency);
         add(currency);
         add(result);
     }
